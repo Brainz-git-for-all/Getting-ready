7a73a93922a9e6bcd8672682ea50c57b94b7f8f4
Brainz-git-for-all <brainzgit@gmail.com>
2026-01-14 16:35:08 +0000

    feat: User classes

---
+++ sprint/Pac/src/main/java/sprint/Pac/User/User.java
@@ -0,0 +1,22 @@
+package sprint.Pac.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class User {
+    private String name;
+    private String role;
+    private String password;
+
+}
---
+++ sprint/Pac/src/main/java/sprint/Pac/User/UserDTO.java
@@ -0,0 +1,16 @@
+package sprint.Pac.User;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserDTO {
+    private String name;
+    private String role;
+}
