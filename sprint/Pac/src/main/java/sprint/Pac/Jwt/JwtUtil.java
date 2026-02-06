bcd7a94f7db546d86a8838585d7a3cc6428aa7b9
Brainz <brainzgit@gmail.com>
2026-02-06 19:46:22 +0000

    sprint security implementation

---
+++ sprint/Pac/src/main/java/sprint/Pac/Jwt/JwtUtil.java
@@ -0,0 +1,62 @@
+package sprint.Pac.Jwt;
+
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    private SecretKey key;
+
+
+    @PostConstruct
+    public void init(){
+       this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateTokenByUserName(String UserName){
+        return Jwts.builder()
+                .subject(UserName)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUserNameFronToken(String token){
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        }
+        catch (Exception e){
+            log.error("jwt validation error");
+        }
+        return false;
+    }
+
+
+
+
+
+}
