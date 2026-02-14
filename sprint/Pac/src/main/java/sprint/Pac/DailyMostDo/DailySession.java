7ebf6250cd4ee14316c977e6a0e1cecc9f32e3a0
Brainz-git-for-all <brainzgit@gmail.com>
2026-02-14 06:00:44 -0800

    boiler code removal

--- sprint/Pac/src/main/java/sprint/Pac/DailyMostDo/DailySession.java
+++ sprint/Pac/src/main/java/sprint/Pac/DailyMostDo/DailySession.java
@@ -1,6 +1,8 @@
 package sprint.Pac.DailyMostDo;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import sprint.Pac.Jwt.User;
 
 import java.time.LocalDate;
@@ -9,6 +11,8 @@ import java.util.List;
 
 
 
+@Setter
+@Getter
 @Entity
 @Table(name = "daily_sessions", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"user_id", "logDate"})
@@ -41,35 +45,4 @@ public class DailySession {
 
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getLogDate() {
-        return logDate;
-    }
-
-    public void setLogDate(LocalDate logDate) {
-        this.logDate = logDate;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<Long> getCompletedHabitIds() {
-        return completedHabitIds;
-    }
-
-    public void setCompletedHabitIds(List<Long> completedHabitIds) {
-        this.completedHabitIds = completedHabitIds;
-    }
 }
