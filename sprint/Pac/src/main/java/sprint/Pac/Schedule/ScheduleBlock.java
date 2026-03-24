3858e6c47b05b451202b564d6c15dd4bd4df0707
Brainz-git-for-all <brainzgit@gmail.com>
2026-03-24 16:28:30 +0000

    feat schedule

---
+++ sprint/Pac/src/main/java/sprint/Pac/Schedule/ScheduleBlock.java
@@ -0,0 +1,13 @@
+package sprint.Pac.Schedule;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class ScheduleBlock {
+
+
+    private Days day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private List<Long> todosId;
+}
